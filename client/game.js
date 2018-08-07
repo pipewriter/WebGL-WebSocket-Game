4961f065ef2351eaa1a1c932ded84305ea2c7835
Parker Diard <prdiard@gmail.com>
2018-08-06 21:40:39 -0400

    deleted dumb stuff

--- client/game.js
+++ client/game.js
@@ -19,38 +19,7 @@
             indexArray.push(i-2, i-1, i);
         }
     }
-    console.log(vertexArray)
-    console.log(indexArray)
-    console.log(new Float32Array(vertexArray))
 
-    // vertexBuffer = gl.createBuffer();
-    // gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
-
-    // gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(vertexArray),gl.STATIC_DRAW);
-    
-    // triangleBuffer = gl.createBuffer();
-    // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, triangleBuffer); //change here
-    
-    
-    // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(indexArray),gl.STATIC_DRAW); //change here
-    
-    // var vertexPositionAttrib = gl.getAttribLocation(shaderProgram, "vertexPosition"); 
-    // gl.enableVertexAttribArray(vertexPositionAttrib);
-    // gl.vertexAttribPointer(vertexPositionAttrib,3,gl.FLOAT,false, 4 * Float32Array.BYTES_PER_ELEMENT ,0); 
-
-    // var vertexTrianglenAttrib = gl.getAttribLocation(shaderProgram, "triangleNumber"); 
-    // gl.enableVertexAttribArray(vertexTrianglenAttrib);
-    // gl.vertexAttribPointer(vertexTrianglenAttrib,1,gl.FLOAT,false, 4 * Float32Array.BYTES_PER_ELEMENT ,3*Float32Array.BYTES_PER_ELEMENT); 
-    
-    // var elapsedUniformLocation = gl.getUniformLocation(shaderProgram, 'elapsed');
-    // var mouseUniformLocation = gl.getUniformLocation(shaderProgram, 'mousePos');
-    
-
-    // const vertData = [
-    //     0.8, 0.8, 10.0,     0.2, 0.3, 3.0, 
-    //     0.2, 0.1, 10.0,     0.2, 0.4, 3.0, 
-    //     0.1, 0.2, 10.0,     0.3, 0.8, 3.0
-    // ];
     const vertData = [
         0, 0, 0.5, 0.5, 0.5, 0.5,
         0, 0.7, 0.2, 0.5, 0.7, 0.5,
@@ -58,9 +27,6 @@
     ];
 
     const indexData = [0, 1, 2];
-    // const indexData = [0, 1, 2, 0, 1, 2, 2, 1, 3, 1, 3, 2, 4, 5, 6];
-    // const indexData = [9, 3, 1, 3, 2, 1];
-    // const indexData = [2, 1, 0, 2, 1,9,3, 9];
 
     const vd2 = [
         -0.5, -0.5, 0.5, 0.5, 0.5, 0.5,
@@ -86,11 +52,8 @@
 
             var seconds = timestamp/1000;
             var elapsed = (seconds%LOOP_CONSTANT)/LOOP_CONSTANT;
-            // gl.uniform1f(elapsedUniformLocation, new Float32Array([elapsed]));
-            // gl.uniform2fv(mouseUniformLocation, new Float32Array([mxVal, myVal]));
 
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            // gl.drawElements(gl.TRIANGLES, indexArray.length, gl.UNSIGNED_SHORT, 0);
             drawTri(vertData, indexData);
             dt2(vd2, id2);
             dt3(vd3, id3);
