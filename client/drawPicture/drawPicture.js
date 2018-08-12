58d8d557d2a6b62d1b381da6da9cc19c2cee596c
pipewriter <31355453+pipewriter@users.noreply.github.com>
2018-08-12 17:29:07 -0400

    scaling successful

--- client/drawPicture/drawPicture.js
+++ client/drawPicture/drawPicture.js
@@ -24,7 +24,6 @@ window.drawpic.stateSchema = {
 window.drawpic.init = async function initDrawPic(imageUrl){
     let img = await initializeImageFromConfig({...window.drawpic.config, imageUrl});
     return function draw({x, y, r, h, w}){
-        let sq2 = Math.sqrt(2);
         let dist = Math.sqrt(Math.pow(h/2, 2) + Math.pow(w/2, 2));
         let a = Math.atan2(h/2, w/2)
         let [a1, a2, a3, a4] = [
@@ -33,14 +32,14 @@ window.drawpic.init = async function initDrawPic(imageUrl){
             a + Math.PI,
             -a
         ];
-        let d1 = y + Math.sin(a1 + r)*sq2*(dist);
-        let d2 = y + Math.sin(a2 + r)*sq2*(dist);
-        let d3 = y + Math.sin(a3 + r)*sq2*(dist);
-        let d4 = y + Math.sin(a4 + r)*sq2*(dist);
-        let e1 = x + Math.cos(a1 + r)*sq2*(dist);
-        let e2 = x + Math.cos(a2 + r)*sq2*(dist);
-        let e3 = x + Math.cos(a3 + r)*sq2*(dist);
-        let e4 = x + Math.cos(a4 + r)*sq2*(dist);
+        let d1 = y + Math.sin(a1 + r)*(dist);
+        let d2 = y + Math.sin(a2 + r)*(dist);
+        let d3 = y + Math.sin(a3 + r)*(dist);
+        let d4 = y + Math.sin(a4 + r)*(dist);
+        let e1 = x + Math.cos(a1 + r)*(dist);
+        let e2 = x + Math.cos(a2 + r)*(dist);
+        let e3 = x + Math.cos(a3 + r)*(dist);
+        let e4 = x + Math.cos(a4 + r)*(dist);
         const vd = [
             e4, d4, 0.9, 1.0, 0.0,
             e3, d3, 0.9, 0.0, 0.0,
--- client/game.js
+++ client/game.js
@@ -24,8 +24,8 @@
             let mp = window.canvas.getMousePos();
             // d4({x: (mp.x*2-1)*16/9, y: -(mp.y*2-1), r, h:  1, w: 1});
             // d4({x: 1*16/10, y: 0.1, r, h:  0.2, w: 0.2});
-            d5({x: 0.5, y: 0.5,r, h:  1, w: 0.2})
-            d4({x: (mp.x*16/9) +0.015, y: (mp.y)+0.015,r, h:  1.02, w: 1.02})
+            d5({x: 0.5, y: 0.5,r, h:  1, w: 1})
+            d4({x: (mp.x*16/9) +0.01, y: (mp.y)+0.01,r, h: 0.02, w: 0.02})
             // r+= 0.05;
             x = Math.sin(r)
             window.requestAnimationFrame(step);
