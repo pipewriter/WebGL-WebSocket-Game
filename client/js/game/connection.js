3405221c40f743050c368b29041284f1bfd82e18
Parker Diard <prdiard@gmail.com>
2018-08-20 22:14:48 -0400

    movement implemented and more

--- client/js/game/connection.js
+++ client/js/game/connection.js
@@ -5,17 +5,29 @@
     // Connection opened
     socket.addEventListener('open', function (event) {
         // do nothin
+        send1();
     });
 
     // Listen for messages
     socket.addEventListener('message', function (event) {
-        console.log('Message from server ', event.data);
+        // console.log('Message from server ', event.data);
+        const data = JSON.parse(event.data);
+        // console.log(data.players[0].x)
+        window.GAME.updatePlayer(data.players[0])
     });
 
     function send1(){
         socket.send(JSON.stringify({name: "parker"}));
     }
-    function send2(){
-        socket.send(JSON.stringify({uvx:1, uvy: 0}));        
-    }
+
+    let lastX = 0;
+    let lastY = 0;
+    setInterval(()=>{
+        let {uvx, uvy} = window.GAME.getPlayerDirection();
+        if(uvx === lastX && uvy === lastY)
+            return;
+        lastX = uvx;
+        lastY = uvy;
+        socket.send(JSON.stringify({uvx, uvy}))
+    }, 17)
 })();
--- client/js/game/game.js
+++ client/js/game/game.js
@@ -1,31 +1,87 @@
 
+let player = {
+    x: 0,
+    y: 0,
+    dx: 0.8,
+    dy: 0.5,
+    uvx: 0,
+    uvy: 0
+};
+
+let npcs = [];
+
+for(let i = 0; i < 50; i++){
+    npcs.push({
+        x: 500 + i * 25,
+        y: 500,
+        dx: -100,
+        dy: -100
+    });
+}
+
+window.GAME = {};
+window.GAME.updatePlayer = function updatePlayer({x, y}) {
+    player.x = x;
+    player.y = y;
+};
+
+window.GAME.updateDirection = function updateDirection({mp}){
+    let [x, y] = [mp.x - 0.5, mp.y - 0.5];
+    let mag = Math.sqrt(x * x + y * y); 
+    let [uvx, uvy] = [x/mag, y/mag];
+    player = {
+        ...player,
+        uvx,
+        uvy
+    }
+};
+
+window.GAME.getPlayerDirection = function getPlayerDirection(){
+    return {
+        uvx: player.uvx,
+        uvy: player.uvy
+    }
+};
+
+window.GAME.adjustDrawCoords = function adjustDrawCoords(){
+    const xDiff = player.x;
+    const yDiff = player.y;
+    npcs.forEach(npc => {
+        npc.dx = (npc.x - xDiff)/100 + player.dx;
+        npc.dy = (npc.y - yDiff)/100 + player.dy;
+        // npc.dy = (npc.y - yDiff)/100;
+        // npc.dx = 0.5;
+        // npc.dy = 0.5;
+    });
+};
+
 (async () => {
-    initializeWebGL;
-
-    const vd3 = [
-        0, 0, 0.9, 1, 0, 0, 0.0, 1.0,
-        0, 1, 0.9, 1, 0, 0, 0.0, 0.0,
-        1, 0, 0.9, 0, 1, 0, 1.0, 1.0,
-        1, 1, 0.9, 0, 1, 0, 1.0, 0.0
-    ];
-    const id3= [0, 1, 2, 1, 2, 3];
-    
-    const d4 = await window.drawpic.init("./assets/images/cursor.png");
+    const drawGuy = await window.drawpic.init("./assets/images/guy.png");
+    const d4 = await window.drawpic.init("./assets/images/face-big.png");
     const d5 = await window.drawpic.init("./assets/images/guy.png");
+    const d6 = await window.drawpic.init("./assets/images/guy.png");
+    const d7 = await window.drawpic.init("./assets/images/guy.png");
 
-    let r = 0;
     function repeatRender(){
         function step(timestamp) {
             var seconds = timestamp/1000;
+            
+            window.GAME.adjustDrawCoords();
 
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             let mp = window.canvas.getMousePos();
-            // d4({x: (mp.x*2-1)*16/9, y: -(mp.y*2-1), r, h:  1, w: 1});
-            // d4({x: 1*16/10, y: 0.1, r, h:  0.2, w: 0.2});
-            d5({x: 0.5, y: 0.5,r, h:  1, w: 1})
-            d4({x: (mp.x*16/9) +0.02, y: (mp.y)+0.02,r, h: 0.04, w: 0.04})
-            // r+= 0.05;
-            x = Math.sin(r)
+            window.GAME.updateDirection({mp});
+            // d4({x: (mp.x*16/9) +0.02, y: (mp.y)+0.02,r, h: 0.04, w: 0.04})
+            // {
+            //     let npc = npcs[0];
+            //     d4({x: npc.dx, y: npc.dy, r: 0, h: 0.2, w: 0.2})
+            //     // console.log(npc)
+            // }
+            npcs.forEach(npc => {
+                d4({x: npc.dx, y: npc.dy, r: 0, h: 0.2, w: 0.2})
+            });
+
+            // d4({x: (mp.x*16/9) +0.02, y: (mp.y)+0.02,r, h: 0.04, w: 0.04})
             window.requestAnimationFrame(step);
         }
         window.requestAnimationFrame(step);
