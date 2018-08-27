85636e9b978684aa26d7f0079931e09935ce83fc
Parker Diard <prdiard@gmail.com>
2018-08-26 20:53:06 -0400

    fixed tab sizes on utils.js

--- client/js/graphics-tool/utils.js
+++ client/js/graphics-tool/utils.js
@@ -1,37 +1,37 @@
 // example: makeRequest('GET', 'http://example.com');
-(function utils(){
+(function utils() {
     window.utils = {};
-    window.utils.makeRequest = function makeRequest (method, url) {
+    window.utils.makeRequest = function makeRequest(method, url) {
         return new Promise(function (resolve, reject) {
-          var xhr = new XMLHttpRequest();
-          xhr.open(method, url);
-          xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-              resolve(xhr.response);
-            } else {
-              reject({
-                status: this.status,
-                statusText: xhr.statusText
-              });
-            }
-          };
-          xhr.onerror = function () {
-            reject({
-              status: this.status,
-              statusText: xhr.statusText
-            });
-          };
-          xhr.send();
+            var xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+            xhr.onload = function () {
+                if (this.status >= 200 && this.status < 300) {
+                    resolve(xhr.response);
+                } else {
+                    reject({
+                        status: this.status,
+                        statusText: xhr.statusText
+                    });
+                }
+            };
+            xhr.onerror = function () {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            };
+            xhr.send();
         });
     }
 
     window.utils.loadImageElement = function loadImageElement(url) {
-        return new Promise(function (resolve, reject){
-          const image = new Image();
-          image.onload = function(){
-            resolve(image);
-          }
-          image.src = url;
+        return new Promise(function (resolve, reject) {
+            const image = new Image();
+            image.onload = function () {
+                resolve(image);
+            }
+            image.src = url;
         });
     }
 })();
