f71090cdb3a19bec3664d17f86e13dd443da48c4
Parker Diard <prdiard@gmail.com>
2018-09-03 16:57:54 -0400

    circle drawn. somehow i need to change the aspect ratio and translations

--- client/assets/shaders/circleFrag.glsl
+++ client/assets/shaders/circleFrag.glsl
@@ -1,6 +1,14 @@
 precision mediump float;
 
+varying vec2 fragPos;
+
+const float radius1 = 0.9;
+const float radius2 = 0.95;
 
 void main(void) {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    float dist = sqrt(fragPos.x * fragPos.x + fragPos.y * fragPos.y);
+    if(dist < radius1 || dist > radius2){
+        discard;
+    }
+    gl_FragColor = vec4(0.2, 0.8, 0.5, 1.0);
 }
--- client/assets/shaders/circleVert.glsl
+++ client/assets/shaders/circleVert.glsl
@@ -5,6 +5,9 @@ attribute vec2 textureCord;
 
 const vec2 aspect = vec2(16.0, 9.0);
 
+varying vec2 fragPos;
+
 void main(void) {
+    fragPos = vec2(vertPos.x, vertPos.y);
     gl_Position = vec4(vertPos, 1.0);
 }
