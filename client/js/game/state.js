8a180e8ae8ad9f046e6a9a100a4bfe3ea55d0893
Parker Diard <prdiard@gmail.com>
2018-09-21 22:42:29 -0400

    skeleton of state functions

---
+++ client/js/game/state.js
@@ -0,0 +1,33 @@
+(() => {
+    function attemptConnection(){
+        
+    }
+
+    function catchUnplayableGame(reason){
+        const errMsg = `Error connecting to server: ${reason}`;
+        renderTitleScreen(errMsg);
+    }
+
+    function renderTitleScreen(msg, finalScore){
+        //... this is the hard part
+        console.log(`rendering title screen ${msg}`);
+    }
+
+    function handleKillInfo(killInfo){
+        const {
+            killerType,
+            reason,
+            finalScore
+        } = killInfo;
+
+        let msg;
+        if(killerType === 0){
+            msg = `You were swallowed by the player: ${reason}`;
+        }else if(killerType === 1){
+            msg = `You were swallowed by the supermassive black hole`;
+        }
+
+        renderTitleScreen(msg);
+    }
+
+})();
